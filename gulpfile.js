ea7f4d1a0d673d32c39d0330980aadf7b0a7de75
Richard van der Dys <richard.vanderdys@gmail.com>
2016-05-21 10:09:30 -0500

    Updated babel and test scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,6 @@
 var gulp = require('gulp');
-var babel = require('gulp-babel');
 var coveralls = require('gulp-coveralls');
-
-gulp.task('compile', function() {
-	return gulp
-		.src(['src/*.js', 'test/spec.js'])
-		.pipe(babel({
-			optional: ['runtime']
-		}))
-		.pipe(gulp.dest('dist'));
-});
-
-gulp.task('debug', ['compile'], function() {
-	gulp.watch(['src/*.js', 'test/*.js'], ['compile']);
-});
 
 gulp.task('coveralls', function() {
 	return gulp.src('./coverage/lcov.info').pipe(coveralls());
 });
-
-gulp.task('default', ['compile']);
