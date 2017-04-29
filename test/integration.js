30b3bfb81d1e8c434faec9ea5cea5533d7e08f55
Mr. Goferito <goferito@gmail.com>
2017-04-30 01:37:37 +0200

    Test info options

--- test/integration.js
+++ test/integration.js
@@ -1,29 +1,40 @@
 import 'should';
+import fs from 'fs';
+import nock from 'nock';
 import wiki from '../src/wiki';
 
-//TODO I leave this test skipped until I do the nock magic trick
-describe.skip('Dublin page integration', () => {
+describe('Dublin page integration', () => {
 
-  //TODO
-  // download whatever files are needed for nock
+  let dublin;
+  before(done => {
+    nock('http://en.wikipedia.org')
+      .get('/w/api.php?prop=info%7Cpageprops&inprop=url&ppprop=disambiguation'
+         + '&titles=Dublin&format=json&action=query&origin=*&redirects=')
+      .once()
+      .reply(200, JSON.parse(fs.readFileSync('./test/data/dublin-page.json')));
 
-  let dublin
-  before((done) => {
     wiki().page('Dublin')
     .then((page) => {
       dublin = page
       done()
     })
+    .catch(done)
   })
 
   it('should get Dublins info', () => {
-    return dublin.info(null, {simplifyDataValues: false}).then((info) => {
-      info.name.should.equal('Dublin')
-      info.gdp.should.equal('US$ 90.1 billion')
+    nock('http://en.wikipedia.org')
+      .get('/w/api.php?prop=revisions&rvprop=content&rvsection=0'
+         + '&titles=Dublin&format=json&action=query&origin=*&redirects=')
+      .once()
+      .reply(200, JSON.parse(fs.readFileSync('./test/data/dublin-info.json')));
+
+    return dublin.info({simplifyDataValues: false}).then((info) => {
+      info.name.should.equal('Dublin');
+      info.gdp.should.equal('US$ 90.1 billion');
       //TODO fix infobox parser to activate this test
       //info.populationTotal.should.equal(553165)
-    })
-  })
+    });
+  });
 
-})
+});
 
